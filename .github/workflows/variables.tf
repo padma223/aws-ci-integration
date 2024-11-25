8b934e17da133e919191b61bf9dcb381eaa98443
padma223 <padmav223@gmail.com>
2024-11-25 13:00:47 +0530

    Update variables.tf

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -1,18 +1,1 @@
-variable "s3_bucket_name" {
-  default     = "padma1024"
-  description = "Name of the s3 bucket"
-  type        = string
-}
-variable "s3_tags" {
-  type = map
 
-  default = {
-    createdby   = "padma"
-    environment = "dev"
-  }
-}
-
-variable "s3_regions" {
-  type    = string
-  default = "eu-west-1"
-}
